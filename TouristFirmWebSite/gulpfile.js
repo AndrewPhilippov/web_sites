e2756a4ef3f2976ea111402517c7091e6f046585
Andrew <a@fandb.pro>
2019-01-27 13:08:20 +0300

    postcss modules added

--- TouristFirmWebSite/gulpfile.js
+++ TouristFirmWebSite/gulpfile.js
@@ -3,6 +3,7 @@ var gulp         = require('gulp'),
     postcss      = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     nested       = require('postcss-nested'),
+    cssImport    = require('postcss-import'),
     cssvars      = require('postcss-simple-vars');
 
 gulp.task('default', function(){
@@ -25,8 +26,8 @@ gulp.task('styles', styles);
 function styles(done) {
   // Added the console.log because otherwise there's no indication that the function ran and putting console.log after the return statement won't run.
   console.log("Styles task runs");
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+  return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
   // Don't need the styles(done) or if (done) done(); because we are returning something in this function now.
   // if(done) done();
