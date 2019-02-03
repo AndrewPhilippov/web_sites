92a77e326575ba31eb429324eb7ac5430a01feda
Andrew <a@fandb.pro>
2019-02-03 22:43:54 +0300

    Started to work on toogle menu

--- TouristFirmWebSite/gulp/tasks/styles.js
+++ TouristFirmWebSite/gulp/tasks/styles.js
@@ -4,15 +4,15 @@ var gulp         = require('gulp'),
     nested       = require('postcss-nested'),
     mixins       = require('postcss-mixins'),
     cssImport    = require('postcss-import'),
-    cssvars      = require('postcss-simple-vars'),
-    browserSync  = require('browser-sync').create();
+    hexrgba      = require('postcss-hexrgba'),
+    cssvars      = require('postcss-simple-vars');
 
 gulp.task('styles', styles);
 function styles() {
   // Added the console.log because otherwise there's no indication that the function ran and putting console.log after the return statement won't run.
   console.log("Styles task runs");
   return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested,  hexrgba, autoprefixer]))
     // To prevent gulp from stop running
     .on('error', function(errorInfo){
       console.log(errorInfo.toString());
