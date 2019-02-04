73daa59bdfb8f0dae1c21d9ccf08ef615ccb785c
Andrew <a@fandb.pro>
2019-02-04 23:47:34 +0300

    Mobile Menu functionality added

--- TouristFirmWebSite/app/assets/scripts/modules/MobileMenu.js
+++ TouristFirmWebSite/app/assets/scripts/modules/MobileMenu.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 
 class MobileMenu{
   constructor(){
-    this.menuIcon = $('.site-header__menu-icon');
-    this.menuContent =$('.site-header__menu-content');
+    this.siteHeader  = $('.site-header');
+    this.menuIcon    = $('.site-header__menu-icon');
+    this.menuContent = $('.site-header__menu-content');
     this.events();
   }
   events(){
@@ -11,6 +12,7 @@ class MobileMenu{
   }
   toggleTheMenu(){
     this.menuContent.toggleClass('site-header__menu-content--is-visible');
+    this.siteHeader.toggleClass('site-header--is-expanded');
   }
 }
 
